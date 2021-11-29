8be3b63ce9e1923f94bc3cd4bcdc7e911ae3d199
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2021-11-29 15:45:21 +0530

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -33,44 +33,4 @@ def setup_desired_cap(desired_cap):
     :param desired_cap:
     :return:
     """
-    if os.environ.get('env') == 'jenkins':
-        #sets the platform type
-        desired_cap["platform"] = desired_cap["operatingSystem"]
-        del desired_cap["operatingSystem"]
-        #sets the browser version
-        desired_cap["version"] = desired_cap["browserVersion"]
-        del desired_cap["browserVersion"]
-        #sets the tunnel name if configured
-        if "LT_TUNNEL_NAME" in os.environ:
-            desired_cap["TunnelName"] = os.environ["LT_TUNNEL_NAME"]
-            desired_cap["tunnel"] = "true"
-    if "tunnel" in desired_cap:
-        if desired_cap["tunnel"].lower() == "true":
-            desired_cap["tunnel"] = True
-        elif desired_cap["tunnel"].lower() == "false":
-            desired_cap["tunnel"] = False
-    #shows the console logs in LT dashboard if set to true
-    if "console" in desired_cap:
-        if desired_cap["console"].lower() == "true":
-            desired_cap["console"] = True
-        elif desired_cap["console"].lower() == "false":
-            desired_cap["console"] = False
-    #shows the network logs in LT dashboard if set to true
-    if "network" in desired_cap:
-        if desired_cap["network"].lower() == "true":
-            desired_cap["network"] = True
-        elif desired_cap["network"].lower() == "false":
-            desired_cap["network"] = False
-    #shows the network logs in LT dashboard if set to true
-    if "visual" in desired_cap:
-        if desired_cap["visual"].lower() == "true":
-            desired_cap["visual"] = True
-        elif desired_cap["visual"].lower() == "false":
-            desired_cap["visual"] = False
-    # shows the video logs in LT dashboard if set to true
-    if "video" in desired_cap:
-        if desired_cap["video"].lower() == "true":
-            desired_cap["video"] = True
-        elif desired_cap["video"].lower() == "false":
-            desired_cap["video"] = False
     return desired_cap
